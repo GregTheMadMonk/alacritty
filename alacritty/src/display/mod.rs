dce32c453abe9fd3659fd5bc0f7c3779e7b25797
unknown <thenry.he@gmail.com>
2024-10-08 11:18:55 -0500

    towel: idk maybe gets the last cursor, whole screen still updates

--- alacritty/src/display/mod.rs
+++ alacritty/src/display/mod.rs
@@ -736,6 +736,7 @@ impl Display {
         search_state: &mut SearchState,
     ) {
         // Collect renderable content before the terminal is dropped.
+
         let mut content = RenderableContent::new(config, self, &terminal, search_state);
         let mut grid_cells = Vec::new();
         for cell in &mut content {
